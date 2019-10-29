fb10acf76a8d30e6d35deed2a1ceba52cd361abc
Viska97 <m.viska97@gmail.com>
2019-10-29 17:17:46 +0300

    refactor lab 2

--- wave-equation/src/main/kotlin/math/labs/we/SinusModel.kt
+++ wave-equation/src/main/kotlin/math/labs/we/SinusModel.kt
@@ -1,13 +1,8 @@
 package math.labs.we
 import kotlin.math.sin
 
-class SinusModel(h: Double, t: Double, T: Double, L: Double): WEModelBase(1.0, h, t, T, L) {
+class SinusModel(h: Double, k: Double, T: Double, L: Double): WEModelBase(1.0, h, k, T, L) {
 
-    override fun f0(x: Double): Double = (L - x) * sin(x)
+    override fun f(x: Double): Double = (L - x) * sin(x)
 
-    override fun g(x: Double): Double = 0.0
-
-    override fun q1(x: Double): Double = 0.0
-
-    override fun q2(x: Double): Double = 0.0
 }
--- wave-equation/src/main/kotlin/math/labs/we/WEModelBase.kt
+++ wave-equation/src/main/kotlin/math/labs/we/WEModelBase.kt
@@ -5,31 +5,31 @@ import kotlin.math.pow
 abstract class WEModelBase(
         var v : Double = 1.0,
         var h : Double = 0.1,
-        @get:JvmName("gett") @set:JvmName("sett") var t : Double = 0.1,
-        @get:JvmName("getT") @set:JvmName("setT") var T : Double = 100.0,
+        var k : Double = 0.1,
+        var T : Double = 100.0,
         var L : Double = 10.0
 ) {
 
-    abstract fun f0(x: Double): Double
-    abstract fun g(x: Double): Double
-    abstract fun q1(x: Double): Double
-    abstract fun q2(x: Double): Double
+    abstract fun f(x: Double): Double
+    open fun g(x: Double): Double = 0.0
+    open fun q1(x: Double): Double = 0.0
+    open fun q2(x: Double): Double = 0.0
 
     fun solve(): List<List<Pair<Double, Double>>> {
         val result = mutableListOf<List<Pair<Double, Double>>>()
-        val Nt = (T/t).toInt()
+        val Nt = (T/k).toInt()
         val Nx = (L/h).toInt()
         val x = linSpace(0.0, L, Nx+1)
-        val c = ((1/v) * t)/h
+        val c = ((1/v) * k)/h
         val up = DoubleArray(Nx+1)
         var u = DoubleArray(Nx+1)
         var um = DoubleArray(Nx+1)
         for(i in 0..Nx){
-            u[i] = f0(x[i])
+            u[i] = f(x[i])
         }
         result.add(x.asIterable().zip(u.asIterable()))
         for(i in 1 until Nx) {
-            up[i] = u[i] + t * g(x[i])
+            up[i] = u[i] + k * g(x[i])
         }
         up[0]=q1(0.0)
         up[Nx]=q2(Nx.toDouble())
